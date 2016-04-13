bf2fccb2b97b5244daa2b356fc0eceb96b646dc0
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-04-13 10:03:54 +0200

    Reduce test-case further

--- CLaSH/Core/TyCon.hs
+++ CLaSH/Core/TyCon.hs
@@ -1,15 +1,11 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
-
 {-# OPTIONS_GHC -ddump-if-trace #-}
-
 module CLaSH.Core.TyCon where
 
-import GHC.Generics
-import Unbound.Generics.LocallyNameless (Alpha)
-
+import Unbound.Generics.LocallyNameless (Alpha (..))
 import {-# SOURCE #-} CLaSH.Core.Type   (TyName)
 
 data AlgTyConRhs
   = NewTyCon TyName
-  deriving (Generic,Alpha)
+
+instance Alpha AlgTyConRhs where
+  isTerm (NewTyCon nm) = isTerm nm
--- CLaSH/Core/Type.hs
+++ CLaSH/Core/Type.hs
@@ -1,13 +1,11 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
-
 module CLaSH.Core.Type where
 
-import GHC.Generics                     (Generic)
-import Unbound.Generics.LocallyNameless (Alpha,Name)
+import Unbound.Generics.LocallyNameless (Alpha (..),Name)
 import CLaSH.Core.TyCon
 
 data Type = VarTy
-  deriving (Generic,Alpha)
 
 type TyName = Name Type
+
+instance Alpha Type where
+  isTerm Type = False
